e3b004f609329c4819f24cea9a7a8193664a867c
keely glenn <glenj392@newschool.edu>
2017-02-17 08:32:59 -0800

    add object sentence and method to GetSentence

--- Objects/Object.cs
+++ Objects/Object.cs
@@ -1,9 +1,20 @@
 using System;
 using System.Collections.Generic;
 
- namespace WordCounter.Objects
- {
-     public class RepeatCounter
-     {
-     }
- }
+namespace WordCounter.Objects
+{
+    public class RepeatCounter
+    {
+        private string _sentence;
+
+        public RepeatCounter(string userSentence)
+        {
+            _sentence = userSentence;
+        }
+
+        public string GetSentence()
+        {
+            return _sentence;
+        }
+    }
+}
